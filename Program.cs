00ca45d0c4b5c071da043cb554807d45234b843b
manly <philippe.lord.temp@gmail.com>
2016-12-11 19:24:33 -0500

    code cleanup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 
@@ -10,55 +12,123 @@ namespace BlockChain {
         static void Main(string[] args) {
             var folder = GetBlocksFolder();
 
-            var files = System.IO.Directory.GetFiles(folder, "*.dat");
+            var files = Directory.GetFiles(folder, "blk*.dat");
 
             // this code is intentionally not parallel to remain simple
             // it used to be, but the bottleneck is IO anyway and wasn't that much faster
-            int block_id = 0;
-            foreach(var file in files) {
-                ParseFile(file, ref block_id);
-            }
+            LogScriptPatterns(files, @"e:\patterns.txt");
+            LogNonStdTransactions(files);
         }
 
-        private static void ParseFile(string file, ref int block_id) {
+
+        #region private static LogNonStdTransactions()
+        /// <summary>
+        ///     Logs all non-standard transactions.
+        ///     This is useful for detecting which blockfiles/block are likely to contain hidden data.
+        /// </summary>
+        private static void LogNonStdTransactions(string[] files) {
             var sb = new StringBuilder();
             var sb_transactions = new StringBuilder();
-
+            string current_file = null;
+            
             try {
-                foreach(var block in Block.ParseAll(file, block_id)) {
-                    block_id++;
-                    int tx_id = 0;
-                    // todo: do something here with the blocks
-                    foreach(var transaction in block.Transactions) {
-                        foreach(var txout in transaction.Outs) {
-                            var text_script = txout.Script.ToString();
-                            if(DataChunkExtractor.Filter(text_script))
-                                continue;
-                            sb.AppendFormat("block {0} - transaction https://blockchain.info/tx/{1}", block.Height, transaction.TransactionHash);
-                            sb.AppendLine();
-                            sb.AppendLine(text_script);
-                            sb.AppendLine();
-                            
-                            sb_transactions.AppendLine(transaction.TransactionHash.ToString());
+                int block_height = 0;
+                foreach(var file in files) {
+                    current_file = file;
+                    foreach(var block in Block.ParseAll(file)) {
+                        block.Height = block_height++;
+                        foreach(var transaction in block.Transactions) {
+                            foreach(var txout in transaction.Outs) {
+                                var text_script = txout.Script.ToString();
+
+                                if(DataChunkExtractor.Filter(text_script))
+                                    continue;
+                                sb.AppendFormat("block {0} - transaction https://blockchain.info/tx/{1}", block.Height, transaction.TransactionHash);
+                                sb.AppendLine();
+                                sb.AppendLine(text_script);
+                                sb.AppendLine();
+
+                                sb_transactions.AppendLine(transaction.TransactionHash.ToString());
+                            }
                         }
-                        tx_id++;
                     }
-                    block_id++;
+                    var folder = Path.Combine(Path.GetDirectoryName(file), "non-std scripts");
+                    if(!Directory.Exists(folder))
+                        Directory.CreateDirectory(folder);
+                    var newfile = Path.Combine(folder, Path.ChangeExtension(Path.GetFileName(file), "txt"));
+                    File.WriteAllText(newfile, sb.ToString());
+
+                    newfile = Path.ChangeExtension(newfile, ".transactions.txt");
+                    File.WriteAllText(newfile, sb_transactions.ToString());
                 }
             } catch(Exception ex) {
-                throw new FormatException($"An error occured reading the file {file}. File might be corrupted or contain extra data.", ex);
+                throw new FormatException($"An error occured reading the file {current_file}. File might be corrupted or contain extra data. This also occurs when reading block files from old version of bitcoin core.", ex);
             }
-
-            var newfile = System.IO.Path.ChangeExtension(file, "") + " - non-std scripts.txt";
-            System.IO.File.WriteAllText(newfile, sb.ToString());
-
-            newfile = System.IO.Path.ChangeExtension(file, "") + " - non-std scripts - transactions.txt";
-            System.IO.File.WriteAllText(newfile, sb_transactions.ToString());
         }
+        #endregion
+        #region private static LogScriptPatterns()
+        /// <summary>
+        ///     Logs all transaction scripts formats, so that this way we can identify 
+        ///     the different ways data might be encoded on the blockchain.
+        /// </summary>
+        private static void LogScriptPatterns(string[] files, string output_pattern_file = @"e:\patterns.txt") {
+            var sb = new StringBuilder();
+            var patterns = new Dictionary<string, ScriptPattern>();
+            string current_file = null;
+
+            try {
+                int block_height = 0;
+                foreach(var file in files) {
+                    current_file = file;
+                    foreach(var block in Block.ParseAll(file)) {
+                        block.Height = block_height++;
+                        foreach(var transaction in block.Transactions) {
+                            foreach(var txout in transaction.Outs) {
+                                var text_script = txout.Script.ToString();
+
+                                var pattern = Helper.DetectScriptPattern(text_script);
+                                ScriptPattern temp;
+                                if(!patterns.TryGetValue(pattern, out temp)) {
+                                    temp = new ScriptPattern() {
+                                        Pattern = pattern,
+                                        Example = text_script,
+                                        TxId = txout.Script.ToString(),
+                                    };
+                                    patterns.Add(pattern, temp);
+                                }
+                                temp.Count++;
+                            }
+                        }
+                    }
+                    File.WriteAllText(output_pattern_file, string.Join(Environment.NewLine, patterns.Select(o => o.Value)));
+                }
+            } catch(Exception ex) {
+                throw new FormatException($"An error occured reading the file {current_file}. File might be corrupted or contain extra data. This also occurs when reading block files from old version of bitcoin core.", ex);
+            }
+        }
+
+        private class ScriptPattern {
+            public long Count;
+            public string Pattern;
+            public string Example;
+            public string TxId;
+            public override string ToString() {
+                var sb = new StringBuilder();
+                sb.Append(this.Count.ToString(CultureInfo.InvariantCulture));
+                sb.Append(": ");
+                sb.AppendLine(this.Pattern);
+                sb.AppendFormat("example: {0}", this.Example);
+                sb.AppendLine();
+                sb.AppendFormat("transaction https://blockchain.info/tx/{0}", this.TxId);
+                sb.AppendLine();
+                return sb.ToString();
+            }
+        }
+        #endregion
 
         private static string GetBlocksFolder() {
             var author_bitcoin_folder = @"E:\BitCoinCore BlockChain\blocks";
-            if(System.IO.Directory.Exists(author_bitcoin_folder))
+            if(Directory.Exists(author_bitcoin_folder))
                 return author_bitcoin_folder;
 
             // change this to your "BitCoin Core" folder. ie: "%appdata%/Bitcoin/data" ?
